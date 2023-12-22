da2d8c09ffc63ccc0c585d7fbf364d9263cc0104
ShunJie <xiaoshunjie@qq.com>
2023-12-22 23:15:12 +0800

    函数的代码复用

--- chapter5/5函数和代码的复用.py
+++ chapter5/5函数和代码的复用.py
@@ -19,12 +19,23 @@ def fact_divided(n, m=1):
     return s // m
 
 
+def fact_multiply(n, *b):
+    s = 1
+    for i in range(1, n + 1):
+        s *= i
+    for item in b:
+        s *= item
+    return s
+
+
 def test1():
     print("5！= {}".format(fact(5)))
     print("4！= {}".format(fact(4)))
     print("4！/ 2 = {}".format(fact_divided(4, 2)))
     print("5！/ 1 = {}".format(fact_divided(5)))
     print("10！/ 5 = {}".format(fact_divided(10, 5)))
+    print("10！* 3 = {}".format(fact_multiply(10, 3)))
+    print("3！* 5 * 4 = {}".format(fact_multiply(3, 5, 4)))
 
 
 test1()

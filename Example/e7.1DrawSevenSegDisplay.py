dc56f0b1ec5a3ddf50f345de923dc43ffeea1089
ShunJie <xiaoshunjie@qq.com>
2023-12-25 23:26:41 +0800

    函数的代码复用+绘制数码管

---
+++ Example/e7.1DrawSevenSegDisplay.py
@@ -0,0 +1,43 @@
+import turtle, datetime
+
+
+# 根据判断画出单段数码管
+def draw_line(draw):
+    turtle.pendown() if draw else turtle.penup()
+    turtle.fd(40)
+    turtle.right(90)
+
+
+# 根据数字绘制一个7段数码管
+def draw_digit(d):
+    draw_line(True) if d in [2, 3, 4, 5, 6, 8, 9] else draw_line(False)
+    draw_line(True) if d in [0, 1, 3, 4, 5, 6, 7, 8, 9] else draw_line(False)
+    draw_line(True) if d in [0, 2, 3, 5, 6, 8, 9] else draw_line(False)
+    draw_line(True) if d in [0, 2, 6, 8] else draw_line(False)
+    turtle.left(90)
+    draw_line(True) if d in [0, 4, 5, 6, 8, 9] else draw_line(False)
+    draw_line(True) if d in [0, 2, 3, 5, 6, 7, 8, 9] else draw_line(False)
+    draw_line(True) if d in [0, 1, 2, 3, 4, 7, 8, 9] else draw_line(False)
+    turtle.left(180)
+    turtle.penup()
+    turtle.fd(20)
+
+
+# 根据字符串输出一句数码管
+def draw_date(date):
+    for c in date:
+        draw_digit(eval(c))
+
+
+def main():
+    turtle.setup(800, 350, 200, 200)
+    turtle.speed("fast")
+    turtle.penup()
+    turtle.fd(-300)
+    turtle.pensize(5)
+    date = datetime.datetime.now().strftime('%Y%m%d')
+    draw_date(date)
+    turtle.hideturtle()
+
+
+main()
--- chapter5/5函数和代码的复用.py
+++ chapter5/5函数和代码的复用.py
@@ -1,8 +1,9 @@
 """
-函数
+5 函数和代码复用
 """
 
 
+# 5.1 函数的定义、调用过程
 def fact(n):
     s = 1
     for i in range(1, n + 1):
@@ -10,13 +11,21 @@ def fact(n):
     return s
 
 
+def test2():
+    # 5.1.3 lambda 函数主要用作一些特定函数或方法的参数
+    f = lambda x, y: x + y
+    print(type(f))
+    print('f(10, 12) = {}'.format(f(10, 12)))
+
+
+# 5.2.1 可选参数和可变参数数量
 def fact_divided(n, m=1):
     # 可选参数传递
     s = 1
     for i in range(1, n + 1):
         s *= i
-    # 双斜杠表示整除
-    return s // m
+    # 双斜杠表示整除,返回元组
+    return s // m, n, m
 
 
 def fact_multiply(n, *b):
@@ -38,4 +47,32 @@ def test1():
     print("3！* 5 * 4 = {}".format(fact_multiply(3, 5, 4)))
 
 
-test1()
+def func(x1, y1, z1, x2, y2, z2):
+    print(x1, x2, y1, y2, z1, z2)
+
+
+def func1(a, b):
+    return b, a * b
+
+
+# 5.2.2 参数的位置和名称传递
+def test3():
+    func(1, 2, 3, 4, 5, 6)
+    func(x2=4, x1=1, y2=5, y1=2, z1=3, z2=6)
+    s = func1('hello~', 2)
+    func2('c')
+    print(ls)
+    # 5.2.3 函数的返回值
+    print(s, type(s))
+
+
+ls = ['f', 'F']
+
+
+def func2(a):
+    ls.append(a)
+
+
+# test1()
+test2()
+test3()

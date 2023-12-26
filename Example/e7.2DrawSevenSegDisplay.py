dc6a6ce83fb44b1633ff40ed932579064d070aec
ShunJie <xiaoshunjie@qq.com>
2023-12-26 23:25:52 +0800

    绘制数码管7.2

--- Example/e7.2DrawSevenSegDisplay.py
+++ Example/e7.2DrawSevenSegDisplay.py
@@ -1,11 +1,22 @@
+# 模块化思维
+# 规则化计算机思维
+# 化繁为简
 import turtle
 import datetime
+import time
+
+
+def draw_gap():
+    turtle.penup()
+    turtle.fd(5)
 
 
 # 根据判断画出单段数码管
 def draw_line(draw):
+    draw_gap()
     turtle.pendown() if draw else turtle.penup()
     turtle.fd(40)
+    draw_gap()
     turtle.right(90)
 
 
@@ -26,18 +37,30 @@ def draw_digit(d):
 
 # 根据字符串输出一句数码管
 def draw_date(date):
+    calendar_str = '年月日'
+    color_str = ['red', 'green', 'blue', 'yellow']
+    i = 0
+    turtle.pencolor(color_str[i])
     for c in date:
-        draw_digit(eval(c))
+        if c == ';' and i < 3:
+            turtle.write(calendar_str[i], font=('Arial', 18, 'normal'))
+            i += 1
+            turtle.pencolor(color_str[i])
+            turtle.fd(50)
+        else:
+            draw_digit(eval(c))
 
 
 def main():
     turtle.setup(800, 350, 200, 200)
     turtle.speed("fast")
     turtle.penup()
-    turtle.fd(-300)
+    turtle.fd(-350)
     turtle.pensize(5)
-    date = datetime.datetime.now().strftime('%Y%m%d')
+    date = datetime.datetime.now().strftime('%Y;%m;%d;')
     draw_date(date)
+    date = time.strftime('%Y%m%d', time.gmtime())
+    print(date)  # 20231226
     turtle.hideturtle()
 
 
--- Ssilicon_valley/1.py
+++ Ssilicon_valley/1.py
@@ -1,7 +1,14 @@
-""" 用python """
+def test1():
+    temp = input("不妨猜一下我想的是哪一个数字：")
+    guess = int(temp)
 
-temp = input("不妨猜一下我想的是哪一个数字：")
-guess = int(temp)
+    if guess == 8:
+        print("Yes")
 
-if guess == 8:
-    print("")
+
+def test2():
+    string1 = '年月日'
+    print(string1[0], string1[1], string1[2])
+
+
+test2()

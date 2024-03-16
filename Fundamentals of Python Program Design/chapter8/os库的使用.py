e94b1dbaca3f8ad0c9384b5b17d181886def31fd
xiaoshunjie <xiaoshunjie@qq.com>
2024-03-16 23:24:57 +0800

    os库的使用与第三方库安装

--- Fundamentals of Python Program Design/chapter8/os库的使用.py
+++ Fundamentals of Python Program Design/chapter8/os库的使用.py
@@ -1,7 +1,11 @@
 """
 os库的使用
+路径操作
+进程管理
+环境参数
 """
 import os
+import os.path as op
 
 
 def test1():
@@ -28,4 +32,21 @@ def test2():
           .format(user, cpu_count))
 
 
-test2()
+def test3():
+    """
+    os.path
+    :return:
+    """
+    filepath = op.abspath("第三方库安装.py")
+    # 归一化路径表示形式，将os的其他路径格式统一
+    filepath2 = op.normpath("D://codeHUb//ad")
+    filepath3 = op.relpath("第三方库安装.py")
+    # filepath = op.abspath("第三方库安装.py")
+
+    print(filepath)
+    print(filepath2)
+    print(filepath3)
+
+
+# test2()
+test3()

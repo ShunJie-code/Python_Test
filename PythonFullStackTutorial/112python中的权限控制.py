5bf6c9e9c93fff9534d2cfaa978c1bc580cc7bfd
xiaoshunjie <xiaoshunjie@qq.com>
2024-03-25 23:13:24 +0800

    类的封装

--- PythonFullStackTutorial/112python中的权限控制.py
+++ PythonFullStackTutorial/112python中的权限控制.py
@@ -1,5 +1,17 @@
 """
 面向对象的三大特征
 封装、继承、和多态
+保护 单下划线
 """
 
+class Student:
+    def __init__(self, name, age, gender):
+        self._name = name
+        self.__age = age
+        self.gender = gender
+
+    def _func1(self):
+        print('子类及本身可以访问')
+
+    def __func2(self):
+        print('只有定义的类可以访问')

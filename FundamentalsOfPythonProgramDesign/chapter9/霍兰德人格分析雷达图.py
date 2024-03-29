bcb7f6496eb02bad7caabb821ea59802981aa4ff
xiaoshunjie <xiaoshunjie@qq.com>
2024-03-29 23:20:10 +0800

    霍兰德人格分析雷达图与python学习展望

---
+++ FundamentalsOfPythonProgramDesign/chapter9/霍兰德人格分析雷达图.py
@@ -0,0 +1,13 @@
+"""
+编程的目标感
+编程的沉浸感
+编程的熟练度
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.rcParams['font.family'] = 'SimHei'
+matplotlib.rcParams['font.sans-serif'] = ['SimHei']
+
+radar_labels = np.array(["研究型"])

cbf1e814b420679821e19eed09ef6832dbb58bd5
xiaoshunjie <xiaoshunjie@qq.com>
2024-03-30 23:15:26 +0800

    霍兰德人格分析雷达图

--- FundamentalsOfPythonProgramDesign/chapter9/霍兰德人格分析雷达图.py
+++ FundamentalsOfPythonProgramDesign/chapter9/霍兰德人格分析雷达图.py
@@ -10,4 +10,6 @@ import matplotlib
 matplotlib.rcParams['font.family'] = 'SimHei'
 matplotlib.rcParams['font.sans-serif'] = ['SimHei']
 
-radar_labels = np.array(["研究型"])
+radar_labels = np.array(["研究型", '艺术型', '社会型', '企业型', '常规型', '现实型'])
+nAttr = 6
+data = np.array([[0.4, 0.32, 0.35, ]])

de72e7e101de874d55b2db0c89c2bbd3f5c947e2
xiaoshunjie <xiaoshunjie@qq.com>
2024-04-11 23:08:55 +0800

    坐标系绘制与图例与阴影

--- FundamentalsOfPythonProgramDesign/chapter9/m9.1PlotTriangle.py
+++ FundamentalsOfPythonProgramDesign/chapter9/m9.1PlotTriangle.py
@@ -4,5 +4,6 @@ import matplotlib.pyplot as plt
 
 x = np.linspace(0, 6, 100)
 y = np.cos(2 * np.pi * x) * np.exp(-x) + 0.8
-plt.plot(x, y, color='r', linewidth=3, linestyle='-')
+# 第三个参数代表曲线的颜色 'k' == 黑色；也可以用color='r'赋值
+plt.plot(x, y, 'k', linewidth=3, linestyle='-')
 plt.show()
---
+++ FundamentalsOfPythonProgramDesign/chapter9/m9.3PlotDarkCoordinate.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0, 10, 1000)
+y = np.cos(2*np.pi*x) * np.exp(-x) + 0.8
+plt.plot(x, y, 'k', label='$exp-decay$', linewidth=3)
+# 设置坐标系的范围
+# plt.axis([0, 6, 0, 1.8])  # 最好用元组类型
+plt.axis((0, 9, 0, 1.8))
+ix = (x > 0.8) & (x < 3)
+plt.fill_between(x, y, 0, where=ix, facecolor='grey', alpha=0.25)
+
+# \mathrm{d} 将斜体变成正体
+plt.text(0.5*(0.8+3), 0.2, r"$\int_a^b f(x)\mathrm{d}x$", horizontalalignment='center')
+plt.legend()
+plt.show()

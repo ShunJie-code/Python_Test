f1a47c957f3b9c58d10ef1a9dfbcaf5050cdca37
xiaoshunjie <xiaoshunjie@qq.com>
2024-04-14 17:19:50 +0800

    python语言程序设计基础完结

--- FundamentalsOfPythonProgramDesign/chapter10/10_网络爬虫与自动化.py
+++ FundamentalsOfPythonProgramDesign/chapter10/10_网络爬虫与自动化.py
@@ -5,5 +5,12 @@ Scrapy: 优秀的网络爬虫框架
 pyspider: 强大的Web页面爬取系统
 
 web 信息提取
+Django: MTV 模型、模板、视图
+Pyramid: 规模适中的Web应用框架
+Flask: 最简单构建web系统的应用框架
 
+python 库之网络应用开发
+WeRoBot: 微信公众号的开发框架
+aip: 百度AI开放平台接口
+MyQR: 二维码生成第三方库
 """
---
+++ FundamentalsOfPythonProgramDesign/chapter11/附录B人机接口与图形界面.py
@@ -0,0 +1,21 @@
+"""
+11 从人机交互到图形艺术：
+
+pyqt5 Riverbank Computing 是pyqt开发商
+wxPython
+PyGObject
+GTK+:跨平台的一种用户界面GUI框架
+Anaconda 采用该库构建GUI
+
+游戏开发
+PyGame
+Panda3D
+cocos2d
+
+虚拟现实
+VR Zero: 在树莓派上开发VR应用的Python库
+Vizard: 通用VR开发引擎
+
+图形艺术
+
+"""
